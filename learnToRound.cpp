198d6b56f89ea1756f1b142596b0f6b3cb3900f0
Cooper Ross <grady_ross@mhu.edu>
2026-03-04 21:39:00 -0500

    Finished pre-rounding

---
+++ learnToRound.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    double gran = 0.004, sRTT;
+
+
+    cout << setprecision(3) << 444.01335;
+
+    /*while (true) {
+        cin >> sRTT;
+
+
+        if (fmod(sRTT, gran) != 0) sRTT = (sRTT - fmod(sRTT, gran)) + gran;
+
+        //sRTT = (sRTT / gran)
+
+        //if (fmod(sRTT, gran) != 0) sRTT += gran - fmod(sRTT, gran);
+        cout << sRTT << endl << endl;
+    }*/
+
+
+    return 0;
+}
--- tcpTimeout.cpp
+++ tcpTimeout.cpp
@@ -1,5 +1,9 @@
 /**
- * This is Cooper testing to see if my new branch exists
+ * Cooper Ross and Thomas Jenkins
+ * March 4th, 2026
+ * This program simulates the timer used in TCP's exponential weighted moving average
+ * (EWMA), taking clock granularity and minimum RTO on the command line and asking for new
+ * sample RTTs to calculate the new RTO.
  */
 #include <iostream>
 #include <string.h>
@@ -7,35 +11,64 @@ using namespace std;
 
 int main (int argc, char * argv[]) {
 
-  if (argc < 3) {
-    cout << "Not enough args correct structure should be "  << argv[0] << ", <granularity>, " <<  "<minimum RTO>, optional debugging flag <0 or 1>" << endl;
-    return 1;
-  }
-  double gran, minRTO, sRTT, devRTT, estRTT, toi;
-  double beta = 0.25;
-  double alpha = 0.125;
-  gran = stod(argv[1]);
-  minRTO = stod(argv[2]);
-  toi = 1;
+    // Error checking
+    if (argc < 3) {
+        cout << "This program takes two arguments:\n" <<
+            "Argument 1: Clock Granularity (as a decimal number. 0.004 is the linux standard)\n" <<
+            "Argument 2: Minimum RTO (as a decimal number. 1 = IETF recommended value, 0.2 is Linux standard, 0 = no minimum)\n";
+            return 1;
+    }
+    if (stod(argv[1]) <= 0) {
+        cout << "Granularity should be more than 0. Exiting." << endl;
+        return 2;
+    }
 
-  cout << "Beginning RTO: " << toi << endl;
+    // Declare clock granularity, minimum RTO, sample RTT, deviation RTT, estimated RTT,
+    // timeout interval, beta, and alpha.
+    double gran, minRTO, sRTT, devRTT, estRTT, toi;
+    double beta = 0.25;
+    double alpha = 0.125;
+    gran = stod(argv[1]);
+    minRTO = stod(argv[2]);
+    toi = 1;
 
-  cout << "Enter the first sample RTT (enter a value <=0 to quit) >";
-  cin >> sRTT;
-  if (sRTT > 0) {
+    cout << endl << "Beginning RTO: " << toi << endl << endl;
+
+    // Ask for first sample RTT, ending the program if a value <= 0 is given
+    cout << "Enter the first sample RTT (enter a value <=0 to quit) >";
+    cin >> sRTT;
+    if (sRTT <= 0) return 0;
+
+    // Calculate toi based on first sample
     devRTT = sRTT / 2;
     estRTT = sRTT;
-    if (gran > (4 * devRTT)) {
-      toi = estRTT + gran;
-    } else {
-      toi = estRTT + (4 * devRTT);
-    }
-  }
+    if (gran > (4 * devRTT)) toi = estRTT + gran;
+    else toi = estRTT + (4 * devRTT);
 
-  while (sRTT > 0) {
+    // Ensure the RTO does not fall below the minimum value and output it
+    if (toi < minRTO) toi = minRTO;
+    cout << "New RTO: " << toi << endl << endl;
+
+    // Ask for next sample RTT before beginning loop
     cout << "Enter the next sample RTT (enter a value <= 0 to quit) >";
     cin >> sRTT;
-  }
+    if (sRTT <= 0) return 0;
 
-  return 0;
+    // Calculate and output the new RTO until the user exits
+    while (sRTT > 0) {
+        devRTT = (1 - beta) * devRTT + beta * abs(sRTT - estRTT);
+        estRTT = (1 - alpha) * estRTT + alpha * sRTT;
+        if (gran > (4 * devRTT)) toi = estRTT + gran;
+        else toi = estRTT + (4 * devRTT);
+
+        // Ensure the RTO does not fall below the minimum value
+        if (toi < minRTO) toi = minRTO;
+
+        cout << "New RTO: " << toi << endl << endl;
+
+        cout << "Enter the next sample RTT (enter a value <= 0 to quit) >";
+        cin >> sRTT;
+    }
+
+    return 0;
 }
